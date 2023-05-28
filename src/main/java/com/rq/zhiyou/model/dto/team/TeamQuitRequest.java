784e147ae7316ee113f53c3160b974bd15b44fb0
JUN <JUN@qq.com>
2023-05-28 18:31:02 +0800

    add 初版完成!

---
+++ src/main/java/com/rq/zhiyou/model/dto/team/TeamQuitRequest.java
@@ -0,0 +1,22 @@
+package com.rq.zhiyou.model.dto.team;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author 若倾
+ * @version 1.0
+ * @description TODO
+ * @date 2023/5/22 15:18
+ */
+@Data
+public class TeamQuitRequest implements Serializable {
+    /**
+     * teamId
+     */
+    private Long teamId;
+
+
+    private static final long serialVersionUID = 1L;
+}
